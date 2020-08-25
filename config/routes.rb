7a2984ce82cdb423a3a3ed9c215ca94f54b22a88
koumake <koumake422@gmail.com>
2020-08-25 17:18:44 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
   root to: 'meetings#index'
   resources :meetings
+  resources :messages
+  resources :rooms
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
