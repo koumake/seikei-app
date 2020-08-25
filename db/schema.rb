02ec28196ce8414a2fb8e2ab5e6041276706ac68
koumake <koumake422@gmail.com>
2020-08-25 14:27:07 +0900

    meetingテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_153955) do
+ActiveRecord::Schema.define(version: 2020_08_25_051914) do
+
+  create_table "meetings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "date_time", null: false
+    t.string "plece", null: false
+    t.string "number", null: false
+    t.text "target_person", null: false
+    t.text "content", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_meetings_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_08_24_153955) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "meetings", "users"
 end
