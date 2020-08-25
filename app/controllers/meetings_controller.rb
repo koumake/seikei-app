3a636a272082de5511809f0ed97ac0bf8c00dd83
koumake <koumake422@gmail.com>
2020-08-25 15:07:37 +0900

    オフ会募集投稿機能（createアクション）

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -6,5 +6,13 @@ class MeetingsController < ApplicationController
   def new
     @meeting = Meeting.new
   end
-  
+  def create
+    Meeting.create(meeting_params)
+  end
+
+  private
+  def meeting_params
+    params.require(:meeting).permit(:title, :date_time, :plece, :number, :target_person, :content).merge(user_id: current_user.id)
+  end
+
 end
