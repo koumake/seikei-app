bdc75792137739bb374cabd4a7604c5bdc9b636a
koumake <koumake422@gmail.com>
2020-08-27 01:12:21 +0900

    レビュー投稿機能実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,10 +8,15 @@ class ReviewsController < ApplicationController
   end
   def create
     @review = Review.new(review_params)
+    if @review.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def review_params
-    params.require(:review).permit(:title, :treatment_sitem, :date, :clinic, :price, :content).merge(user_id: current_user.id)
+    params.require(:review).permit(:title, :treatment_site, :date, :clinic, :docter, :price, :content).merge(user_id: current_user.id)
   end
 end
