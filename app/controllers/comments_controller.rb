e8e4842dc5f14ff9425eb7bbbdcc8ac96d1233a9
koumake <koumake422@gmail.com>
2020-08-27 03:32:25 +0900

    コメント機能実装完了

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    Comment.create(comment_params)
+    comment = Comment.create(comment_params)
     redirect_to "/reviews/#{comment.review.id}"
   end
 
