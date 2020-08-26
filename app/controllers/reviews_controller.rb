b93f1769d1a38b8a75521a52d9a8946356c174f6
koumake <koumake422@gmail.com>
2020-08-26 23:33:17 +0900

    コントローラー実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,17 @@
 class ReviewsController < ApplicationController
 
   def index
+    @reviews = Review.all
   end
   def new
+    @review = Review.new
   end
   def create
+    @review = Review.new(review_params)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:title, :treatment_sitem, :date, :clinic, :price, :content).merge(user_id: current_user.id)
   end
 end
