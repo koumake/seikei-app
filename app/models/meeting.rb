e8f4eee4ba83143a2b4f00b5bfc4180046d2d0c6
koumake <koumake422@gmail.com>
2020-09-02 00:26:23 +0900

    meetingモデルバリデーション記述

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,3 +1,12 @@
 class Meeting < ApplicationRecord
     belongs_to :user
+
+    with_options presence: true do
+        validates :title
+        validates :date_time
+        validates :plece
+        validates :number
+        validates :target_person
+        validates :content
+    end
 end
